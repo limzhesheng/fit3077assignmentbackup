6b29dbacaad4a7d806b96c6ffed289e22177d0d9
zlim0027 <zlim0027@student.monash.edu>
2022-04-21 19:39:25 +1000

    begin writing constructor to create TestingSite from JSONObject

--- server/assignment2/src/main/java/com/fit3077/assignment2/jsonEntities/TestingSite.java
+++ server/assignment2/src/main/java/com/fit3077/assignment2/jsonEntities/TestingSite.java
@@ -1,9 +1,11 @@
 package com.fit3077.assignment2.jsonEntities;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import com.fit3077.assignment2.config.CliConfig;
 import org.json.*;
+import org.modelmapper.ModelMapper;
 
 public class TestingSite {
     protected String id;
@@ -32,4 +34,49 @@ public class TestingSite {
             this.additionalInfo = new JSONObject("{}");
         }
     }
+
+    private static final String ADDRESS_KEY = "address";
+    private static final String BOOKINGS_KEY = "bookings";
+
+    public TestingSite(JSONObject obj) {
+        ModelMapper modelMapper = new ModelMapper();
+        this.address = modelMapper.map(obj.getJSONObject(ADDRESS_KEY), Address.class);
+        JSONArray bookingsRawData = obj.getJSONArray(BOOKINGS_KEY);
+        for (int i = 0; i < bookingsRawData.length(); i++) {
+            this.addBooking(new Booking(bookingsRawData.getJSONObject(i)));
+        }
+    }
+
+    public void addBooking(Booking b) {
+        if (this.bookings == null) {
+            this.bookings = new ArrayList<>();
+        }
+        this.bookings.add(b);
+    }
+
+    /**
+     * TestingSite nested items:
+     * TestingSite --|
+     *               |--> Address
+     *               |--> List<Booking> --|
+     *                                    |--> Patient
+     *                                    |--> List<CovidTest> --|
+     *                                                           |--> Patient
+     *                                                           |--> Receptionist
+     * 
+     * When using ModelMapper you may need to map JSON to each of the classes from bottom up.
+     * For example:
+     * for (Object b : testsite.getJSONArray("bookings")) {
+     *     JSONObject bk = new JSONObject(b);
+     *     Patient p1 = new ModelMapper().map(bk.getJSONObject("patient"), Patient.class);
+     *     for (Object c : new JSONObject(b).getJSONArray("covidTests")) {
+     *         JSONObject ct = new JSONObject(c);
+     *         Patient p2 = new ModelMapper().map(ct.getJSONObject("patient"), Patient.class);
+     *         Receptionist r = new ModelMapper().map(ct.getJSONObject("receptionist"), Receptionist.class);
+     *         // create CovidTest object with p2 and r, and put into array
+     *     }
+     *     // create Booking object with p1, and put into array
+     * }
+     */
+
 }
